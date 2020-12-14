bebe24f1e25d900926d0489cc363b484aae23adc
igiboi <36388765+igiboi@users.noreply.github.com>
2020-12-14 17:19:41 -0500

    created a modules folder with a user.js file inside

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const {check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register user
@@ -14,12 +18,45 @@ check(
   "Please enter a password with 6 or more characters"
 ).isLength({min: 6})
 ],
-(req, res) => {
+ async (req, res) => {
   const errors = validationResult(req);
   if(!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
+  
+  const { name, email, password } = req.body;
+
+  try {
+  // See if user exists 
+    let user = await User.findOne({ email });
+
+    if(user) {
+      res.status(400).json({ errors: [{ msg: 'User already exists'}] });
+    }
+  // Get users gravatars
+    const avatar = gravatar.url(email, {
+      s:'200',
+      r:'pg',
+      d:'mm'
+    })
+
+    user = new User({
+      name,
+      email, 
+      avatar, 
+      password
+    });
+  // Encrypt passwod
+
+  // Return jswonwebtoken
   res.send('User route')
-});
+
+} catch(err) {
+  console.log(err.message);
+  res.status(500).send('Server error');
+    }
+
+  }
+);
 
 module.exports = router;
