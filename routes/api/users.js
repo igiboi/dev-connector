273da9f9f17cc02f797eb3dfbce463956516a466
igiboi <36388765+igiboi@users.noreply.github.com>
2020-12-14 17:53:43 -0500

    Update user.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,53 +9,57 @@ const User = require('../../models/User');
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
-router.post('/', [check('name', 'Name is required')
-.not()
-.isEmpty(),
-check('email', 'Please include a valid email').isEmail(),
-check(
-  'password',
-  "Please enter a password with 6 or more characters"
-).isLength({min: 6})
-],
- async (req, res) => {
-  const errors = validationResult(req);
-  if(!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  
-  const { name, email, password } = req.body;
-
-  try {
-  // See if user exists 
+router.post(
+  "/",
+  [
+    check("name", "Name is required!").not().isEmpty(),
+    check("email", "Please include a valid email! ").isEmail(),
+    check("password", "Enter a password with 6 or more letters!").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+ 
+    //Check if the user exist
     let user = await User.findOne({ email });
-
-    if(user) {
-      res.status(400).json({ errors: [{ msg: 'User already exists'}] });
+ 
+    try {
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exist!" }] });
+      }
+ 
+      //Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+ 
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+ 
+      //Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+ 
+      //Return Jsonwebtoken
+      res.send("User register");
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server error.");
     }
-  // Get users gravatars
-    const avatar = gravatar.url(email, {
-      s:'200',
-      r:'pg',
-      d:'mm'
-    })
-
-    user = new User({
-      name,
-      email, 
-      avatar, 
-      password
-    });
-  // Encrypt passwod
-
-  // Return jswonwebtoken
-  res.send('User route')
-
-} catch(err) {
-  console.log(err.message);
-  res.status(500).send('Server error');
-    }
-
   }
 );
 
