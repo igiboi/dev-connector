9ff055e11ffd418ef5e320b55ff2dc6bee30f8c8
igiboi <36388765+igiboi@users.noreply.github.com>
2020-12-17 21:22:53 -0500

    update profile.js
    added a check validation result for postman to validate users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const {check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/profile');
 const User = require('../../models/User');
@@ -21,4 +22,17 @@ router.get('/me', auth, async (req, res) => {
   }
 });
 
-module.exports = router;
+
+// @route   POST api/profile/me
+// @desc    Create or update user profile
+// @access  Private
+router.post('/',[ auth, [check('status', 'Status is required').not().isEmpty(),
+  check('skills', 'Skills is required').not().isEmpty()]], async (req, res) => {
+    const errors = validationResult(req);
+    // if there are errors
+    if(!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  });
+
+module.exports = router;            
