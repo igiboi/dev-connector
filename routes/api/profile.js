cc84c87fd8e79f3231b88157cacc39bb27098c48
igiboi <36388765+igiboi@users.noreply.github.com>
2021-01-08 17:46:58 -0500

    Github Repo

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const {check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/profile');
 const User = require('../../models/User');
-const profile = require('../../models/profile');
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
@@ -299,5 +300,27 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
     res.status(500).send('Server Error');
  }
 });
+
+// @route   GET api/profile/github/:username
+// @desc    Get  user repos from Github
+// @access  Public
+router.get('/github/:username', async (req, res) => {
+  try {
+    const uri = encodeURI(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+    const headers = {
+      'user-agent': 'node.js',
+      Authorization: `token ${config.get('githubToken')}`
+    };
+
+    const gitHubResponse = await axios.get(uri, { headers });
+    return res.json(gitHubResponse.data);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(404).json({ msg: 'No Github profile found' });
+  }
+});
+
 
 module.exports = router;            
