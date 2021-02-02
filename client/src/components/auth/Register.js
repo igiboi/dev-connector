7c9f9cd799e7397bf07f514713e83c0f3e02caf5
igiboi <36388765+igiboi@users.noreply.github.com>
2021-02-02 16:02:21 -0500

    Added remove alert dispatch
    added authentication for users register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import React, { Fragment, useState } from 'react';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
 
-export const Register = () => {
+export const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -17,9 +20,9 @@ export const Register = () => {
     const onSubmit = async e => {
       e.preventDefault();
       if(password != password2) {
-        console.log('Passwords do not match');
+        setAlert('Password do not match', 'danger');
       } else {
-       
+        console.log('SUCCESS');
       }
     }
 
@@ -62,4 +65,8 @@ export const Register = () => {
   );
 };
 
-export default Register
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register)
